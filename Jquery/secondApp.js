f6648da5e28fe3d454270b315ae0caa053c3d276
AlexanderPaul1983 <alexanderpaul@gmx.de>
2024-04-24 14:04:18 +0200

    finished

--- Jquery/secondApp.js
+++ Jquery/secondApp.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $(".btn").click(function() {
         $(".container").toggleClass("bg-danger");
     });
+
+    $('#carouselExampleAutoplaying').carousel({
+        interval: 4000
+    });
+    $('#carouselExampleAutoplaying').on('slid.bs.carousel', function () {
+        $('.hide').hide(); 
+    });
+    
 })
